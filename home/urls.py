902809e90076143c1686566356e669cb9c5fd119
Hassanprogramming <117660191+Hassanprogramming@users.noreply.github.com>
2023-07-30 17:46:31 +0330

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', HomeView.as_view(), name='Home'),
+    path('factor/', FactorView.as_view(), name='factor'),
+    path('add_factor/', AddFactorView.as_view(), name='add_factor')
 ]

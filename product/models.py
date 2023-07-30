473228ab7862744dbc7e7693653d7af87af96ee0
Hassanprogramming <117660191+Hassanprogramming@users.noreply.github.com>
2023-07-30 17:41:17 +0330

    Update models.py

--- product/models.py
+++ product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -8,20 +9,21 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-
+    
+    
 class Product(models.Model):
     name = models.CharField(verbose_name="نام محصول", max_length=100)
     dec = models.CharField(verbose_name="توضیخات", max_length=500, blank=True, null=True)
     Category = models.ForeignKey(Category, verbose_name="دسته", on_delete=models.CASCADE)
     price = models.FloatField()
-    img = models.ImageField(verbose_name="تصویر محصول")
+    # img = models.ImageField(verbose_name="تصویر محصول")
     
     def __str__(self):
         return self.name
-    @property
-    def imageURL(self):
-        try:
-            url = self.img.url
-        except:
-            url = ''
-        return url
+    # @property
+    # def imageURL(self):
+    #     try:
+    #         url = self.img.url
+    #     except:
+    #         url = ''
+    #     return url

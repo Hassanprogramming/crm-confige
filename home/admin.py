ef824eacdb97a121492df20d2967a53e91865eed
Hassanprogramming <117660191+Hassanprogramming@users.noreply.github.com>
2023-08-15 10:03:03 +0330

    Update admin.py

--- home/admin.py
+++ home/admin.py
@@ -5,10 +5,19 @@ from .models import *
 
 class FactorAdmin(admin.ModelAdmin):
     # Customize how the Factor model is displayed in the admin panel
-    list_display = ('name', 'user', 'Category', 'date', 'dec', 'checks', 'number', 'price', 'total_price')
-    list_filter = ('user', 'Category', 'checks')
+    list_display = ('name', 'Category', 'date', 'customer', 'checks', 'number', 'price', 'total_price')
+    list_filter = ('Category', 'checks')
     search_fields = ('name', 'user__name', 'Category__name')
     ordering = ('name',)
     # ...
 
 admin.site.register(Factor, FactorAdmin)
+
+class ServiceAdmin(admin.ModelAdmin):
+    # Customize how the Factor model is displayed in the admin panel
+    list_display = ('name_service', 'date', 'customer', 'checks', 'total_price')
+    search_fields = ('name_service', 'user__name')
+    ordering = ('name_service',)
+    # ...
+
+admin.site.register(service, ServiceAdmin)

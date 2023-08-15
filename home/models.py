0dc5f8057ecc72c28e2c595751beab8e58804f0b
Hassanprogramming <117660191+Hassanprogramming@users.noreply.github.com>
2023-08-15 10:02:34 +0330

    Update models.py

--- home/models.py
+++ home/models.py
@@ -1,25 +1,61 @@
 from django.db import models
 from account.models import *
+from product.models import *
+from account.models import *
+
 # Create your models here.
 
 class Factor(models.Model):
+    class Meta:
+        verbose_name = "فاکتور ها "
+        verbose_name_plural = "فاکتور ها "
+        
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(verbose_name="نام محصول", max_length=100)
-    Category = models.CharField(verbose_name="دسته", max_length=100)
-    date = models.DateTimeField(verbose_name="تاریخ ثبت")
-    dec = models.CharField(verbose_name="توضیحات", max_length=500)
+    customer = models.ForeignKey(Customer , verbose_name="مشتری", on_delete=models.CASCADE)
+    name = models.ForeignKey(Product, verbose_name="نام محصول", max_length=100, on_delete=models.CASCADE, blank=True, null=True)
+    Category = models.ForeignKey(Category, verbose_name="دسته", max_length=100, on_delete=models.CASCADE)
+    date = models.DateTimeField(verbose_name="تاریخ ثبت", auto_now_add=True)
+    dec = models.TextField(verbose_name="توضیحات", max_length=500)
     checks = models.BooleanField(verbose_name="پرداخت شده؟")
     number = models.FloatField(verbose_name="تعداد")
-    price = models.FloatField()
-    total_price = models.FloatField()
-    # img = models.ImageField()
+    price = models.FloatField(verbose_name="قیمت", blank=True, null=True)
+    total_price = models.FloatField(verbose_name="مبلغ کل")
+    img = models.ImageField(upload_to="images/", blank=True, null=True)
     
     def __str__(self):
-        return self.name,(str.user)
-    # @property
-    # def imageURL(self):
-    #     try:
-    #         url = self.img.url
-    #     except:
-    #         url = ''
-    #     return url
+        return str(self.name)
+    @property
+    def imageURL(self):
+        try:
+            url = self.img.url
+        except:
+            url = ''
+        return url
+
+
+class service(models.Model):
+    class Meta:
+        verbose_name = "خدمات"
+        verbose_name_plural = "خدمات"
+        
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer , verbose_name="مشتری", on_delete=models.CASCADE)
+    name_service = models.CharField(verbose_name="نام خدمات", max_length=100)
+    date = models.DateTimeField(verbose_name="تاریخ ثبت", auto_now_add=True)
+    dec = models.TextField(verbose_name="توضیحات", max_length=500)
+    checks = models.BooleanField(verbose_name="پرداخت شده؟")
+    number = models.FloatField(verbose_name="تعداد", blank=True, null=True)
+    price = models.FloatField(verbose_name="قیمت", blank=True, null=True)
+    total_price = models.FloatField(verbose_name="مبلغ کل")
+    img = models.ImageField(upload_to="images/", blank=True, null=True)
+    
+    def __str__(self):
+        return self.name_service
+    @property
+    def imageURL(self):
+        try:
+            url = self.img.url
+        except:
+            url = ''
+        return url
+    

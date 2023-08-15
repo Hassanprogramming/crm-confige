c8e3470e5c4900974b8e6b355cb67d4c7beae400
Hassanprogramming <117660191+Hassanprogramming@users.noreply.github.com>
2023-08-15 09:57:24 +0330

    Update views.py

--- account/views.py
+++ account/views.py
@@ -1,14 +1,46 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import *
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import View
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib import messages
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user_model
+
 
 
 # Create your views here.
+class AccountView(View):
+    def get(self, request):
+        users = User.objects.all()
+        return render(request, 'account/accounts.html', {'users':users})
+
+class UserAccountView(View):
+    def get(self, request, pid):
+        users = get_object_or_404(User, pk=pid)
+        form = User_detail_Form(instance=users)
+        context = {
+            'form': form,
+            'users': users
+        }
+        return render(request, 'account/user_profile.html', context)
+    
+    def post(self, request, pid):
+        users = get_object_or_404(User, pk=pid)
+        form = User_detail_Form(request.POST, request.FILES, instance=users)
+        if form.is_valid():
+            form.save()
+            return redirect('account')
+        else:
+            context = {
+                'form': form,
+                'users':users
+            }
+        return render(request, 'account/user_profile.html', context)
+
 
 class RegisterView(View):
     def get(self, request):
@@ -71,9 +103,69 @@ class ProfileEditView(View):
         form = ProfileEdit_Form(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            return redirect('profile_edit')
+            return redirect('Home')
         else:
             context = {
                 'form': form
             }
             return render(request, 'account/profile.html', context)
+        
+        
+@method_decorator(login_required, name='dispatch')
+class DeleteAccountView(LoginRequiredMixin, View):
+    def get(self, request):
+        # Delete the user account and log them out
+        user = request.user
+        user.delete()
+        return redirect('Home')
+    
+    
+class CustomerView(View):
+    def get(self, request):
+        customer = Customer.objects.all()
+        return render(request, 'account/customer.html', {'customer':customer})
+
+class Edit_CustomerView(View):
+    def get(self, request, pid):
+        customer = get_object_or_404(Customer, pk=pid)
+        form = EditeCustomer(instance=customer)
+        context = {
+            'form': form,
+            'customer': customer
+        }
+        return render(request, 'account/edit_customer.html', context)
+    
+    def post(self, request, pid):
+        customer = get_object_or_404(Customer, pk=pid)
+        form = EditeCustomer(request.POST, request.FILES, instance=customer)
+        if form.is_valid():
+            form.save()
+            return redirect('customer')
+        else:
+            context = {
+                'form': form,
+                'customer':customer
+            }
+        return render(request, 'account/edit_customer.html', context)
+
+class Add_CustomerView(View):
+    def get(self, request):
+        customer = Customer.objects.all()
+        form = AddCustomer()
+        context = {
+            'form': form,
+            'customer': customer,
+        }
+        return render(request, 'account/add_customer.html', context)
+
+    def post(self, request):
+        form = AddCustomer(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            print(form)
+            return HttpResponseRedirect(reverse('customer'))
+        else:
+            context = {
+                'form': form
+            }
+            return render(request, 'account/add_customer.html', context)

11d36f0a3d558f54a57e8dd536ac34c0c5cf72e4
Hassanprogramming <117660191+Hassanprogramming@users.noreply.github.com>
2023-08-15 09:57:57 +0330

    Update forms.py

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from .models import *
-from django.contrib.auth.forms import AuthenticationForm
 
 
 
@@ -18,3 +17,18 @@ class ProfileEdit_Form(forms.ModelForm):
     class Meta:
         model = User
         fields = ['name', 'phone', 'email']
+        
+class User_detail_Form(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["phone", "email", "name"]
+        
+class AddCustomer(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ["name", "company_name", "date", "img"]
+        
+class EditeCustomer(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ["name", "company_name", "date", "img"]

d7062afbb12ed164b55ad64591c325e9330e11a7
Hassanprogramming <117660191+Hassanprogramming@users.noreply.github.com>
2023-08-15 09:58:11 +0330

    Update admin.py

--- account/admin.py
+++ account/admin.py
@@ -1,23 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
+from .models import *
 
-class UserAdmin(BaseUserAdmin):
-    list_display = ('name', 'phone', 'email', 'is_admin', 'is_second_admin', 'is_third_admin')
-    list_filter = ('is_admin', 'is_second_admin', 'is_third_admin')
-    search_fields = ('name', 'phone', 'email')
-    ordering = ('name',)
-    fieldsets = (
-        (None, {'fields': ('name', 'phone', 'email', 'password', 'user_permissions', 'groups')}),
-        ('Admin Privileges', {'fields': ('is_admin', 'is_second_admin', 'is_third_admin')}),
-        ('Important Dates', {'fields': ('date_created',)}),
-    )
-    readonly_fields = ('date_created',)
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('name', 'phone', 'email', 'password1', 'password2', 'is_admin', 'is_second_admin', 'is_third_admin', 'user_permissions', 'groups'),
-        }),
-    )
-
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
+admin.site.register(Customer)

614b00fa5672ada097ff21574d8200f97c44e9ce
Hassanprogramming <117660191+Hassanprogramming@users.noreply.github.com>
2023-08-15 10:02:47 +0330

    Update forms.py

--- home/forms.py
+++ home/forms.py
@@ -5,9 +5,29 @@ from .models import *
 class Add_Factor_Form(forms.ModelForm):
     class Meta:
         model = Factor
-        fields = ['user', 'name', 'Category', 'date', 'dec', 'checks', 'number', 'price', 'total_price']
+        fields = ['name', 'customer', 'Category', 'dec', 'checks', 'number', 'price', 'total_price', 'img']
         
 class FactorForm(forms.ModelForm):
     class Meta:
         model = Factor
         fields = '__all__'
+        
+class Edit_Factor_Form(forms.ModelForm):
+    class Meta:
+        model = Factor
+        fields = ['name', 'customer', 'Category', 'dec', 'checks', 'number', 'price', 'total_price', 'img']
+        
+class Add_Service_Form(forms.ModelForm):
+    class Meta:
+        model = service
+        fields = ['name_service', 'customer', 'dec', 'checks', 'number', 'price', 'total_price', 'img']
+        
+class ServiceForm(forms.ModelForm):
+    class Meta:
+        model = service
+        fields = '__all__'
+        
+class Edit_Service_Form(forms.ModelForm):
+    class Meta:
+        model = service
+        fields = ['name_service', 'customer', 'dec', 'checks', 'number', 'price', 'total_price', 'img']

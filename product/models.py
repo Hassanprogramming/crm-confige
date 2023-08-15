d14ec599e6884c0615df0eeefc1108202e2121b0
Hassanprogramming <117660191+Hassanprogramming@users.noreply.github.com>
2023-08-15 10:07:10 +0330

    Update models.py

--- product/models.py
+++ product/models.py
@@ -4,6 +4,10 @@ from django.urls import reverse
 # Create your models here.
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = "بالا دسته"
+        verbose_name_plural = "بالا دسته"
+        
     name = models.CharField(verbose_name="نام دسته", max_length=100)
     display = models.BooleanField(verbose_name="نمایش داده شود؟")
 
@@ -12,18 +16,22 @@ class Category(models.Model):
     
     
 class Product(models.Model):
+    class Meta:
+        verbose_name = "محصولات"
+        verbose_name_plural = "محصولات"
+        
     name = models.CharField(verbose_name="نام محصول", max_length=100)
     dec = models.CharField(verbose_name="توضیخات", max_length=500, blank=True, null=True)
     Category = models.ForeignKey(Category, verbose_name="دسته", on_delete=models.CASCADE)
     price = models.FloatField()
-    # img = models.ImageField(verbose_name="تصویر محصول")
+    img = models.ImageField(upload_to='images/',verbose_name="تصویر محصول", blank=True, null=True)
     
     def __str__(self):
         return self.name
-    # @property
-    # def imageURL(self):
-    #     try:
-    #         url = self.img.url
-    #     except:
-    #         url = ''
-    #     return url
+    @property
+    def imageURL(self):
+        try:
+            url = self.img.url
+        except:
+            url = ''
+        return url

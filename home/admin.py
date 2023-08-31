e60ca4866106d7bea7863b7d393f781e89e0e9e6
Hassanprogramming <117660191+Hassanprogramming@users.noreply.github.com>
2023-08-31 11:40:38 +0330

    Update admin.py

--- home/admin.py
+++ home/admin.py
@@ -6,18 +6,20 @@ from .models import *
 class FactorAdmin(admin.ModelAdmin):
     # Customize how the Factor model is displayed in the admin panel
     list_display = ('name', 'Category', 'date', 'customer', 'checks', 'number', 'price', 'total_price')
-    list_filter = ('Category', 'checks')
-    search_fields = ('name', 'user__name', 'Category__name')
-    ordering = ('name',)
+    list_filter = ('Category', 'checks', 'date')
+    search_fields = ('name', 'user__name', 'Category__name', 'date')
+    ordering = ('name','date')
     # ...
+    def formatted_date(self, obj):
+        return obj.jcreated()  # Assuming jcreated is the method that converts to Jalali date
 
 admin.site.register(Factor, FactorAdmin)
 
 class ServiceAdmin(admin.ModelAdmin):
     # Customize how the Factor model is displayed in the admin panel
     list_display = ('name_service', 'date', 'customer', 'checks', 'total_price')
-    search_fields = ('name_service', 'user__name')
-    ordering = ('name_service',)
+    search_fields = ('name_service', 'user__name', 'date')
+    ordering = ('name_service','date')
     # ...
 
 admin.site.register(service, ServiceAdmin)

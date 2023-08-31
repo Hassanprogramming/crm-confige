e03770ba7d579afb4c1b9daf1f275a43a94fec5f
Hassanprogramming <117660191+Hassanprogramming@users.noreply.github.com>
2023-08-31 11:45:04 +0330

    Update urls.py

--- product/urls.py
+++ product/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('products/<int:pid>/', Edit_ProductsView.as_view(), name='edit_products'),
     path('add_products/', Add_ProductsView.as_view(), name='add_products'),
     path('add_category/', Add_CategoryView.as_view(), name='add_category'),
+    path('products/delete/<int:pk>/', ProductDeleteView.as_view(), name='delete_product'),
+    path('category/delete/<int:pid>/', CategorytDeleteView.as_view(), name='delete_category'),
 ]

4df9351ad7b57e9a93acb9e0c133a84ffb3e18b1
Hassanprogramming <117660191+Hassanprogramming@users.noreply.github.com>
2023-08-31 11:40:11 +0330

    Update forms.py

--- home/forms.py
+++ home/forms.py
@@ -1,33 +1,22 @@
 from django import forms
 from .models import *
 
-
 class Add_Factor_Form(forms.ModelForm):
     class Meta:
         model = Factor
-        fields = ['name', 'customer', 'Category', 'dec', 'checks', 'number', 'price', 'total_price', 'img']
-        
-class FactorForm(forms.ModelForm):
-    class Meta:
-        model = Factor
-        fields = '__all__'
-        
+        fields = ['name', 'dec_2', 'customer', 'Category', 'dec', 'checks', 'number', 'price', 'total_price', 'img']
+
 class Edit_Factor_Form(forms.ModelForm):
     class Meta:
         model = Factor
-        fields = ['name', 'customer', 'Category', 'dec', 'checks', 'number', 'price', 'total_price', 'img']
-        
+        fields = ['name', 'dec_2', 'customer', 'Category', 'dec', 'checks', 'number', 'price', 'total_price', 'img']
+
 class Add_Service_Form(forms.ModelForm):
     class Meta:
         model = service
-        fields = ['name_service', 'customer', 'dec', 'checks', 'number', 'price', 'total_price', 'img']
-        
-class ServiceForm(forms.ModelForm):
-    class Meta:
-        model = service
-        fields = '__all__'
-        
+        fields = ['name_service', 'dec_2', 'customer', 'dec', 'checks', 'number', 'price', 'total_price', 'img']
+
 class Edit_Service_Form(forms.ModelForm):
     class Meta:
         model = service
-        fields = ['name_service', 'customer', 'dec', 'checks', 'number', 'price', 'total_price', 'img']
+        fields = ['name_service', 'dec_2', 'customer', 'dec', 'checks', 'number', 'price', 'total_price', 'img']

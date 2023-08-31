d935fad0c6967b6d9115a730d29feb624655cc09
Hassanprogramming <117660191+Hassanprogramming@users.noreply.github.com>
2023-08-31 11:44:50 +0330

    Update views.py

--- product/views.py
+++ product/views.py
@@ -1,9 +1,12 @@
 from django.views import View
+from django.views.generic import DeleteView
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
 from .forms import *
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 # Create your views here.
@@ -26,8 +29,11 @@ class Edit_ProductsView(View):
     def post(self, request, pid):
         product = get_object_or_404(Product, pk=pid)
         form = Add_Product_Form(request.POST, request.FILES, instance=product)
+        print(form.data)
         if form.is_valid():
             form.save()
+            print(request.POST)
+            print(form.cleaned_data)
             return redirect('products')
         else:
             context = {
@@ -50,7 +56,6 @@ class Add_ProductsView(View):
         form = Add_Product_Form(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            print(form)
             return HttpResponseRedirect(reverse('products'))
         else:
             context = {
@@ -77,3 +82,38 @@ class Add_CategoryView(View):
             }
             print(form)
             return render(request, 'products/add-category.html', context)
+        
+        
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'products/delete_product_confirmation.html'  # Create a template for confirmation
+    success_url = reverse_lazy('products')  # URL to redirect to after successful deletion
+    
+    @method_decorator(login_required)
+    def get(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        return render(request, self.template_name, {'product': product})
+    
+    @method_decorator(login_required)
+    def post(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        product.delete()
+        return redirect(self.success_url)        
+        
+        
+class CategorytDeleteView(DeleteView):
+    model = Category
+    template_name = 'products/category_delete_confirmation.html'  # Create a template for confirmation
+    success_url = reverse_lazy('products')  # URL to redirect to after successful deletion
+    
+    @method_decorator(login_required)
+    def get(self, request, pid):
+        category = get_object_or_404(Category, pk=pid)
+        return render(request, self.template_name, {'category': category})
+    
+    @method_decorator(login_required)
+    def post(self, request, pid):
+        category = get_object_or_404(Category, pk=pid)
+        category.delete()
+        return redirect(self.success_url)        
+        
